f8719c22cb43873e9bb5a6794d87a3da84913e85
cirodamicooo04 <cirodamico04@gmail.com>
2026-04-18 12:25:50 +0200

    added security to travel service

--- travel-service/src/main/java/it/roadies/travel_service/controller/TestController.java
+++ travel-service/src/main/java/it/roadies/travel_service/controller/TestController.java
@@ -1,5 +1,8 @@
 package it.roadies.travel_service.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,5 +13,16 @@ public class TestController {
     @GetMapping("/test")
     public String test() throws InterruptedException {
         return "Hello World!";
+    }
+
+    @GetMapping("/public/test")
+    public String publicTest() {
+        return "Public Hello World!";
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping("/private/test")
+    public String privateTest(@AuthenticationPrincipal Jwt jwt) {
+        return "Private Hello World from " + jwt.getClaim("preferred_username");
     }
 }
