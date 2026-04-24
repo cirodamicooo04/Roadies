2b1cb32a74992611c1ad54f45066de5c31355e9c
cirodamicooo04 <cirodamico04@gmail.com>
2026-04-24 11:25:54 +0200

    create crud apis for standalone activities

---
+++ travel-service/src/main/java/it/roadies/travel_service/services/ActivityService.java
@@ -0,0 +1,52 @@
+package it.roadies.travel_service.services;
+
+import it.roadies.travel_service.data.dao.ActivityRepository;
+import it.roadies.travel_service.data.dto.request.ActivityCreateRequest;
+import it.roadies.travel_service.data.dto.response.ActivityResponse;
+import it.roadies.travel_service.data.entity.Activity;
+import it.roadies.travel_service.data.entity.ActivityDeparture;
+import it.roadies.travel_service.data.mapper.ActivityMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class ActivityService {
+    private final ActivityMapper activityMapper;
+    private final ActivityRepository activityRepository;
+
+    private void validateActivity(Activity activity){
+        if (activity.getTravel() != null) throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Standalone activity can't have a travel");
+
+        for (ActivityDeparture departure : activity.getDepartures()){
+            if (!departure.getStartTimestamp().isBefore(departure.getEndTimestamp())){
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Departure start date must be before end date");
+            }
+        }
+    }
+
+    @Transactional
+    public ActivityResponse createActivity(ActivityCreateRequest request, String ownerId){
+        Activity activity = activityMapper.toEntity(request);
+        activity.setOwnerId(ownerId);
+
+        activityRepository.save(activity);
+        return activityMapper.toResponse(activity);
+    }
+
+    public ActivityResponse getActivityById(UUID id){
+        Activity activity = activityRepository.findById(id).orElseThrow(() -> new RuntimeException("Activity not found"));
+        return activityMapper.toResponse(activity);
+    }
+
+    public void deleteActivityById(UUID id, String ownerId){
+        Activity activity = activityRepository.findById(id).orElseThrow(() -> new RuntimeException("Activity not found"));
+        if (!activity.getOwnerId().equals(ownerId)) throw new ResponseStatusException(HttpStatus.FORBIDDEN, "Not allowed to delete this activity");
+        activityRepository.deleteById(id);
+    }
+}
