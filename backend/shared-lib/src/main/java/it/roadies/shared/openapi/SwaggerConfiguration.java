9d7da15b4a0aab1b32179a93875ec72bf977d2f7
crxstqn <lprcst04h30c588y@studenti.unical.it>
2026-07-04 16:01:39 +0200

    fix swagger ui

--- backend/shared-lib/src/main/java/it/roadies/shared/openapi/SwaggerConfiguration.java
+++ backend/shared-lib/src/main/java/it/roadies/shared/openapi/SwaggerConfiguration.java
@@ -4,10 +4,13 @@ import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+
+import java.util.List;
 
 @Configuration
 @ConditionalOnClass(name = "io.swagger.v3.oas.models.OpenAPI")
@@ -15,6 +18,7 @@ public class SwaggerConfiguration {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
+                .servers(List.of(new Server().url("/")))
                 .addSecurityItem(new SecurityRequirement().addList("BearerAuth"))
                 .components(new Components()
                         .addSecuritySchemes("BearerAuth",
